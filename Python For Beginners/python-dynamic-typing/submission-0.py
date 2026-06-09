f8201c53fa6b6339a29aa9337967d63e9017559a
Amitva <amitvapal@gmail.com>
2026-06-09 12:41:06 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,12 @@
+variable = 10
+varBool = False
+print(type(variable))
+
+print(type(variable * 1.0))
+
+print(type(varBool))
+
+print(type(str(variable)))
+
+variable = [1,2,3]
+print(type(variable))
